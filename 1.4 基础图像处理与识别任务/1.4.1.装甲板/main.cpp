53f525af832a1e80f2785ad5640b4d34d059a6c4
Courteous121 <2507554712@qq.com>
2022-10-01 13:15:41 +0800

    first commit

---
+++ 1.4 基础图像处理与识别任务/1.4.1.装甲板/main.cpp
@@ -0,0 +1,201 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <opencv2/highgui/highgui_c.h>
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/opencv.hpp>
+#include <cmath>
+
+using namespace std;
+
+using namespace cv;
+
+vector<Vec4i> hierarchy;
+
+vector<vector<Point>> t1;
+
+Point2f vertices1[4];
+Point2f vertices2[4];
+
+double cameraD[3][3] = {{
+                            1.6041191539594568e+03,
+                            0.,
+                            6.3983687194220943e+02,
+                        },
+                        {
+                            0.,
+                            1.6047833493341714e+03,
+                            5.1222951297937527e+02,
+                        },
+                        {
+                            0.,
+                            0.,
+                            1.,
+                        }};
+Mat cameraMatrix(3, 3, cv::DataType<double>::type, cameraD);
+
+double distC[5] = {-6.4910709385278609e-01, 8.6914328787426987e-01, 5.1733428362687644e-03, -4.1111054148847371e-03, 0.};
+
+Mat distCoeffs(5, 1, cv::DataType<double>::type, distC);
+string convertToString(double d)
+{
+    ostringstream os;
+    if (os << d)
+        return os.str();
+}
+
+class light
+{
+private:
+    vector<vector<Point>> t0;
+    RotatedRect rect1; // rect1是旋转最小矩形
+    Point2f vertices3[4];
+    Rect brect;
+    vector<RotatedRect> rectbox0; //筛选出的光条会放在里面
+
+public:
+    light(vector<vector<Point>> t) // t是装脚点的向量
+    {
+        t0 = t;
+    }
+    vector<RotatedRect> getrectbox(); //筛选光条的函数
+};
+vector<RotatedRect> light::getrectbox()
+{
+    for (int i = 0; i < t0.size(); i++)
+    {
+        rect1 = minAreaRect(t0[i]);
+
+        rect1.points(vertices3);
+        brect = rect1.boundingRect();
+        double m5 = rect1.size.width > rect1.size.height ? rect1.size.width : rect1.size.height;
+        double n5 = rect1.size.width < rect1.size.height ? rect1.size.width : rect1.size.height;
+        double m = m5 / n5;
+        double n = brect.height / brect.width;
+        if (m > 0 && m < 8 && n > 0 && n < 8 && brect.area() > 50 && rect1.size.area() > 50) // m在4左右
+        {
+
+            rectbox0.push_back(rect1);
+        }
+    }
+    return rectbox0;
+}
+
+int main()
+{
+    vector<Point3f> objp;
+    objp.clear();
+    objp.push_back(Point3f(0, 0, 0)); //顺时针，第一个点在左上
+    objp.push_back(Point3f(240, 0, 0));
+    objp.push_back(Point3f(240, 100, 0)); // 2.4  1
+    objp.push_back(Point3f(0, 100, 0));
+
+    VideoCapture cap("任务二 装甲板.avi");
+    vector<Point> prebox;
+    while (1)
+    {
+        Mat img, imghsv, img2;
+        cap >> img;
+
+        cvtColor(img, imghsv, COLOR_BGR2HSV);
+
+        inRange(imghsv, Scalar(0, 85, 40), Scalar(35, 255, 255), img2);
+
+        // findContours(img2, t1, hierarchy, RETR_LIST, CV_CHAIN_APPROX_NONE);
+        findContours(img2, t1, hierarchy, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+
+        vector<RotatedRect> rectbox;
+
+        light s1(t1);
+        rectbox = s1.getrectbox(); // rectbox里面是筛选出来的光条
+
+        vector<Point2f> imgp;
+
+        for (int i = 0; i < static_cast<int>(rectbox.size()) - 1; i++)
+        {
+            rectbox[i].points(vertices1);
+            rectbox[i + 1].points(vertices2);
+
+            float m1 = rectbox[i].size.height > rectbox[i].size.width ? rectbox[i].size.height : rectbox[i].size.width;
+            float n1 = sqrt(pow((vertices1[3].x - vertices2[3].x), 2) + pow((vertices1[3].y - vertices2[3].y), 2));
+            float m = n1 / m1;
+            float n = n1 * m1;
+            double m2 = abs(vertices2[3].x - vertices1[1].x);
+            double n2 = abs(vertices2[3].y - vertices1[1].y);
+            double m0 = m2 / n2;
+            if (m > 0 && m < 8 && n < 40000 && n > 2000)
+            {
+
+                line(img, vertices1[0], vertices2[2], Scalar(0, 0, 255), 1);
+                line(img, vertices1[2], vertices2[0], Scalar(0, 0, 255), 1);
+                double x1 = (vertices1[0].x + vertices2[2].x) / 2;
+                double y1 = (vertices1[0].y + vertices2[2].y) / 2;
+                circle(img, Point(x1, y1), 5, Scalar(0, 255, 255), -1);
+
+                prebox.push_back(Point(x1, y1));
+                if (vertices2[0].x >= vertices1[0].x)
+                {
+                    imgp.push_back(vertices1[2]);
+                    imgp.push_back(vertices2[2]);
+                    imgp.push_back(vertices2[0]);
+                    imgp.push_back(vertices1[0]);
+                }
+                else
+                {
+                    imgp.push_back(vertices2[2]);
+                    imgp.push_back(vertices1[2]);
+                    imgp.push_back(vertices1[0]);
+                    imgp.push_back(vertices2[0]);
+                }
+            }
+        }
+        Point pre;
+        int n = prebox.size() - 1;
+
+        if (n - 1 >= 0)
+        {
+            pre.x = 2 * prebox[n].x - prebox[n - 1].x;
+            pre.y = 2 * prebox[n].y - prebox[n - 1].y;
+            circle(img, pre, 5, Scalar(255, 255, 255), -1);
+        }
+        Mat rvecs = Mat::zeros(3, 1, CV_64FC1);
+        Mat tvecs = Mat::zeros(3, 1, CV_64FC1);
+        Mat objm, imgm;
+        Mat(objp).convertTo(objm, CV_32F);
+        if (imgp.size() == 4)
+        {
+            solvePnP(objm, imgp, cameraMatrix, distCoeffs, rvecs, tvecs);
+            Mat rotm;
+            Rodrigues(rvecs, rotm);
+            double a1 = atan2(rotm.at(2, 3), rotm.at(3, 3));
+            double c2 = sqrt(rotm.at(1, 1) * rotm.at(1, 1) + rotm.at(1, 2) * rotm.at(1, 2));
+            double a2 = atan2(-rotm.at(1, 3), c2);
+            double a3 = atan2(rotm.at(1, 2), rotm.at(1, 1));
+
+            // double tx=tvecs.at<double>(0,0);
+            // double ty=tvecs.at<double>(1,0);
+            // double tz=tvecs.at<double>(2,0);
+            // double angxoz=atan(ty/tx)*57.3;
+            // double angxoy=atan(tz/ty)*57.3;
+            // double angyoz=atan(tx/tz)*57.3;
+            string A11 = convertToString(a1);
+            string A22 = convertToString(a2);
+            string A33 = convertToString(a3);
+            putText(img, A11, Point(0, 30), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 1); // angle必须是string数据类型
+            putText(img, A22, Point(0, 60), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 1); // angle必须是string数据类型
+            putText(img, A33, Point(0, 90), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 1); // angle必须是string数据类型
+
+            // cout<<"平移向量："<<tvecs<<endl;
+            // cout<<angxoy<<"度  "<<angxoz<<"度  "<<angyoz<<"度"<<endl;
+        }
+
+        imshow("1", img);
+        // imshow("2",img2);
+
+        if (waitKey(100) == 27)
+            if (waitKey(0) == 27)
+                break;
+    }
+
+    return 0;
+}
---
+++ 1.4 基础图像处理与识别任务/1.4.2.能量机关/main.cpp
@@ -0,0 +1,225 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <opencv2/highgui/highgui_c.h>
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/opencv.hpp>
+#include <cmath>
+
+using namespace std;
+
+using namespace cv;
+vector<Mat> channels;
+Mat imgb, imgb1;
+vector<Vec4i> hierarchy;
+
+vector<vector<Point>> t1;
+Point2f vertices1[4];
+// vector <vector<Point3f>> objp;
+// objp.clear();
+// objp.push_back(Point3f(0,0,0));//顺时针，第一个点在左上
+// objp.push_back(Point3f(100,0,0));
+// objp.push_back(Point3f(100,110,0));
+// objp.push_back(Point3f(0,110,0));
+
+// Mat camera_matrix(3,3,CV_64F);
+// Mat distortion_coefficients(1,5,CV_64F);
+
+//  camera_matrix=(Mat_<double>(3,3)<<1.6041191539594568e+03, 0., 6.3983687194220943e+02, 0.,
+//         1.6047833493341714e+03, 5.1222951297937527e+02, 0., 0., 1.);
+//  distortion_coefficients(Mat_<double>(1,5)<<-6.4910709385278609e-01, 8.6914328787426987e-01,
+//        5.1733428362687644e-03, -4.1111054148847371e-03, 0.);
+
+// double a[3][3]={1.6041191539594568e+03, 0., 6.3983687194220943e+02, 0.,1.6047833493341714e+03, 5.1222951297937527e+02, 0., 0., 1.};
+// Mat camera_matrix(3,3,CV_64F,a);
+// double b[5][1]={-6.4910709385278609e-01, 8.6914328787426987e-01,5.1733428362687644e-03, -4.1111054148847371e-03, 0.};
+// Mat distortion_coefficients(5,1,CV_64F,b);
+
+double cameraD[3][3] = {{
+                            1.6041191539594568e+03,
+                            0.,
+                            6.3983687194220943e+02,
+                        },
+                        {
+                            0.,
+                            1.6047833493341714e+03,
+                            5.1222951297937527e+02,
+                        },
+                        {
+                            0.,
+                            0.,
+                            1.,
+                        }};
+double distC[5] = {-6.4910709385278609e-01, 8.6914328787426987e-01, 5.1733428362687644e-03, -4.1111054148847371e-03, 0.};
+
+Mat cameraMatrix(3, 3, cv::DataType<double>::type, cameraD);
+Mat distCoeffs(5, 1, cv::DataType<double>::type, distC);
+
+string convertToString(double d)
+{
+    ostringstream os;
+    if (os << d)
+        return os.str();
+}
+
+class light
+{
+private:
+    vector<vector<Point>> t0;
+    RotatedRect rect1;
+    Point2f vertices3[4];
+    Rect brect;
+    vector<RotatedRect> rectbox0;
+
+public:
+    light(vector<vector<Point>> t)
+    {
+        t0 = t;
+    }
+    vector<RotatedRect> getrectbox();
+};
+
+vector<RotatedRect> light::getrectbox()
+{
+    for (int i = 0; i < t0.size(); i++)
+    {
+        rect1 = minAreaRect(t0[i]);
+
+        rect1.points(vertices3);
+        brect = rect1.boundingRect();
+        double m5 = rect1.size.width > rect1.size.height ? rect1.size.width : rect1.size.height;
+        double n5 = rect1.size.width < rect1.size.height ? rect1.size.width : rect1.size.height;
+        double m = m5 / n5;
+        double n = brect.height / brect.width;
+        // if (m > 3 && m < 8 && n > 0 && n < 6)
+
+        if (m > 0 && m < 2.5 && rect1.size.area() > 100 && rect1.size.area() < 2000)
+        {
+
+            rectbox0.push_back(rect1);
+        }
+    }
+    return rectbox0;
+}
+
+int main()
+{
+    vector<Point3f> objp;
+    objp.clear();
+    objp.push_back(Point3f(0, 0, 0)); //顺时针，第一个点在左上
+    objp.push_back(Point3f(100.0f, 0, 0));
+    objp.push_back(Point3f(100.0f, 110.0f, 0));
+    objp.push_back(Point3f(0, 110.0f, 0));
+    // objp.push_back(Point3f(50.0f,55.0f,0));//顺时针，第一个点在左上
+    // objp.push_back(Point3f(50.0f,0,0));
+    VideoCapture cap("任务三 能量机关.avi");
+    Mat img;
+
+    Point c0;
+    while (1)
+    {
+        vector<Point2f> imgp;
+        vector<Point> cbox;
+        cap >> img;
+        split(img, channels);
+        imgb = channels.at(0);
+        threshold(imgb, imgb1, 170, 255, THRESH_BINARY); //运用了通道分离加二值化，但是调处的灰度图不够明显，后期调试考虑hsv
+        findContours(imgb1, t1, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE);
+
+        vector<RotatedRect> rectbox;
+
+        light s1(t1);
+
+        rectbox = s1.getrectbox();
+        for (int i = 0; i < static_cast<int>(rectbox.size()) - 1; i++)
+        {
+
+            rectbox[i].points(vertices1);
+            double x = (vertices1[0].x + vertices1[2].x) / 2;
+            double y = (vertices1[0].y + vertices1[2].y) / 2;
+            Point c = Point(x, y);
+            if (rectbox[i].size.area() < 1000)
+            {
+                c0 = c;
+                imgp.push_back(vertices1[1]);
+                imgp.push_back(vertices1[2]);
+                imgp.push_back(vertices1[3]);
+                imgp.push_back(vertices1[0]);
+                Point2f m1, m2;
+                m1.x = (vertices1[1].x + vertices1[3].x) / 2;
+                m1.y = (vertices1[1].y + vertices1[3].y) / 2;
+                m2.x = (vertices1[1].x + vertices1[3].x) / 2;
+                m2.y = vertices1[1].y;
+                // imgp.push_back(m1);
+                // imgp.push_back(m2);
+            }
+            else
+            {
+                cbox.push_back(c);
+            }
+            circle(img, c, 5, Scalar(255, 255, 255), -1);
+
+            for (int j = 0; j < 4; j++)
+            {
+
+                line(img, vertices1[j], vertices1[(j + 1) % 4], Scalar(255, 0, 255), 3, CV_AA);
+            }
+        }
+
+        if (static_cast<int>(cbox.size()) - 1 >= 0)
+            for (int i = 0; i < static_cast<int>(cbox.size()) - 1; i++)
+            {
+                if ((cbox[i].x - c0.x) != 0)
+                {
+
+                    double angle = atan2((cbox[i].y - c0.y), (cbox[i].x - c0.x)); // atan接收一个参数
+                    double angle1 = angle * 57.3;
+                    // cout<<angle1<<endl;
+                    string Angle = convertToString(angle1);
+                    cout << angle1 << "   " << Angle << endl;
+                    putText(img, Angle, cbox[i], FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 2); // angle必须是string数据类型
+                }
+
+                else
+                {
+                    if (cbox[i].y > c0.y)
+                        cout << "90" << endl;
+                    else
+                        cout << "-90" << endl;
+                }
+
+                Mat objm, imgm;
+                Mat(objp).convertTo(objm, CV_32F);
+                // Mat(imgp).convertTo(imgm,CV_32F);
+                Mat rvec, tvec;
+                // Mat rotM(3,3,CV_64F);
+                // Rodrigues(rotM,rvec);
+                Mat rvecs = Mat::zeros(3, 1, CV_64FC1);
+                Mat tvecs = Mat::zeros(3, 1, CV_64FC1);
+                if (imgp.size() == 4)
+                {
+                    solvePnP(objm, imgp, cameraMatrix, distCoeffs, rvecs, tvecs);
+                    double tx = tvecs.at<double>(0, 0);
+                    double ty = tvecs.at<double>(1, 0);
+                    double tz = tvecs.at<double>(2, 0);
+                    double dis = sqrt(tx * tx + ty * ty + tz * tz);
+                    cout << "距离：" << dis << endl;
+                    string DIS = convertToString(dis);
+                    putText(img, DIS, Point(0, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 2); // angle必须是string数据类型
+                }
+                // solvePnP(objp,imgp,camera_matrix,distortion_coefficients,rvec,tvec,SOLVEPNP_EPNP );
+
+                // double tx=tvec.at<double>(0,0);
+                // double ty=tvec.at<double>(1,0);
+                // double tz=tvec.at<double>(2,0);
+                // double dis=sqrt(tx*tx+ty*ty+tz*tz);
+                // cout<<"距离："<<dis<<endl;
+            }
+
+        imshow("1", img);
+
+        if (waitKey(100) == 27)
+            if (waitKey(0) == 27)
+                break;
+    }
+}
